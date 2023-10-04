3293c9433a4e2027b54367116cb29ef7c5fd1372
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-10-04 10:23:13 -0300

    Update Principal.java

--- Factory/Principal.java
+++ Factory/Principal.java
@@ -1,1 +1,9 @@
+public class Principal{
+  public static void main(String args[]){
+    //Padrão Utilizando método Factory
+    DispositivoFactory disp = new DispositivoEletronicoFactory();
+    Dispositivo dispositivo = disp.criarDispositivo("Smartphone", new Caracteristica("laranja", 500, 12.6));
 
+    
+  }
+}
