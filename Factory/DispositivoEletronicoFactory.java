b8a92aa9fb8996d163c1b4fff52e34957cf7e01b
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-10-04 10:19:12 -0300

    Update DispositivoEletronicoFactory.java

--- Factory/DispositivoEletronicoFactory.java
+++ Factory/DispositivoEletronicoFactory.java
@@ -1,5 +1,13 @@
 public class DispositivoEletronicoFactory extends DispositivoFactory{
   public Dispositivo criarDispositivo(String tipo, Caracteristica carac){
-
-  }
+    if(tipo == "Smartphone")
+        return new Smartphone(carac);
+    else if(tipo == "Notebook")
+        return new Notebook(carac);
+    else if(tipo == "Tablet")
+        return new Tablet(carac);
+    else
+        return NULL;
+    
+  }  
 }
