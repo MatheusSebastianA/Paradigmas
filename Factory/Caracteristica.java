64819aa3c04f39312f87454b9e5bbb572932f507
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-10-04 10:25:29 -0300

    Create Caracteristica.java

---
+++ Factory/Caracteristica.java
@@ -0,0 +1,12 @@
+public class Caracteristica{
+  //Atributos
+  private String cor;
+  private int pesoGrama;
+  private double polegadasTela;
+
+  //Construtores
+  
+  //Métodos Get e Set
+
+  //Métodos
+}
