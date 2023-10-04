e4adc01064c962824e3f3d2935633ab199644cc3
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-10-04 10:12:50 -0300

    Create Tablet.java

---
+++ Factory/Tablet.java
@@ -0,0 +1,12 @@
+public class Tablet extends DispositivoEletronico{
+  //Atributos
+
+  //Construtores
+
+  //Métodos Get e Set
+
+  //Métodos
+  public void ligarDispositivoTablet(){
+    S.o.p("Ligando Tablet");
+  }
+}
