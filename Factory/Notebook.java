080671cea0599c390c7846853fcc51d3c4fd77ed
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-10-04 10:12:01 -0300

    Create Notebook.java

---
+++ Factory/Notebook.java
@@ -0,0 +1,12 @@
+public class Notebook extends DispositivoEletronico{
+  //Atributos
+
+  //Construtores
+
+  //Métodos Get e Set
+
+  //Métodos
+  public void ligarDispositivoNotebook(){
+    S.o.p("Ligando Notebook");
+  }
+}
